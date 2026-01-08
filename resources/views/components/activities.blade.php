66f5780169602f606a93b08838930a5104099a9b
Bumpyy <fsghassan2429d@gmail.com>
2026-01-08 10:58:44 +0700

    update activities font size

--- resources/views/components/activities.blade.php
+++ resources/views/components/activities.blade.php
@@ -5,7 +5,7 @@
         class="m-auto mb-4 flex w-dvw flex-col items-center bg-activities bg-cover bg-center bg-no-repeat px-4 py-10 text-white max-lg:-mx-4 md:px-8 md:py-20 lg:-mx-[calc((100vw-64rem)/2)]">
 
         <x-container>
-            <x-event-list-title class="!m-auto text-white after:!bg-bni-gold md:!text-5xl">
+            <x-event-list-title class="!m-auto text-white after:!bg-bni-gold">
                 OUR EVENTS & ACTIVITIES
             </x-event-list-title>
         </x-container>
