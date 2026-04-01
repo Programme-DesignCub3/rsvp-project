ab80aed8768cb639fd84a379e21052a9f0658b92
julians22 <dabnerjulian@yahoo.com>
2026-04-01 12:24:37 +0700

    build: update assets and styles
    - Update compiled asset hashes in manifest.json
    - Rebuild app.css and theme styles
    - Update app layout to reference new build files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,9 @@
 
     @include('includes.analytics')
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=montserrat:300,400,500,600,700,800,900" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Winky+Sans:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
     @livewireStyles
 
@@ -39,7 +40,7 @@
 </head>
 
 <body class="relative bg-white antialiased">
-    <div class="sticky top-0 z-20 bg-red-bni">
+    <div class="top-0 z-20 sticky bg-red-bni">
 
         @php
             $navItems = [
@@ -71,9 +72,9 @@
             ];
         @endphp
 
-        <div class="mx-auto w-full max-w-none px-4 lg:max-w-5xl lg:px-0">
+        <div class="mx-auto px-4 lg:px-0 w-full max-w-none lg:max-w-5xl">
             <x-container>
-                <nav class="relative z-10 w-auto" x-data="{
+                <nav class="z-10 relative w-auto" x-data="{
                     width: 0,
                     height: 0,
                     windowWidth: 0,
@@ -91,17 +92,17 @@
                             'xl': 1280,
                             '2xl': 1536,
                         };
-                
+
                         const isNumber = typeof width === 'number'
-                
+
                         const matchingBreakpoint = isNumber ?
                             width :
                             Object.keys(breakpoints).find(key => key === width)
-                
+
                         if (!matchingBreakpoint) {
                             return false;
                         }
-                
+
                         return isNumber ? this.windowWidth >= matchingBreakpoint : this.windowWidth >= breakpoints[matchingBreakpoint];
                     },
                     navigationMenuLeave() {
@@ -129,15 +130,15 @@
                     windowHeight = window.innerHeight;
                     windowWidth = window.innerWidth;">
                     <div
-                        class="relative mx-auto flex w-full max-w-none items-center justify-between px-8 max-lg:py-4 lg:max-w-5xl xl:px-0">
-                        <div class="w-full max-w-24 brightness-0 invert">
+                        class="relative flex justify-between items-center mx-auto px-8 xl:px-0 max-lg:py-4 w-full max-w-none lg:max-w-5xl">
+                        <div class="brightness-0 invert w-full max-w-24">
                             <img src="{{ asset('img/logo-bni.png') }}" alt="">
                         </div>
 
                         {{-- /* ****************** Desktop Main Nav ****************** */ --}}
                         <template x-if="isBreakpoint('lg')">
                             <ul
-                                class="flex h-full flex-wrap items-center justify-end gap-[clamp(0.2rem,4vw,2.3rem)] font-semibold uppercase text-white max-lg:hidden max-sm:items-end md:mx-16">
+                                class="max-lg:hidden flex flex-wrap justify-end items-center max-sm:items-end gap-[clamp(0.2rem,4vw,2.3rem)] md:mx-16 h-full font-semibold text-white uppercase">
                                 @foreach ($navItems as $item)
                                     <li class="py-[clamp(0.5rem,4vw,2.3rem)] lg:py-8"
                                         @if (!empty($item['subItems'])) @mouseover="() => {
@@ -159,22 +160,22 @@
                         </template>
 
                         <template x-if="!isBreakpoint('lg')">
-                            <div class="relative z-50 h-auto w-auto" x-data="{
+                            <div class="z-50 relative w-auto h-auto" x-data="{
                                 slideOverOpen: false
                             }">
 
                                 <x-lucide-menu
-                                    class="inline-flex h-10 basis-10 cursor-pointer items-center justify-center rounded-md border px-4 py-2 text-sm font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-neutral-200/60 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 lg:hidden"
+                                    class="lg:hidden inline-flex justify-center items-center disabled:opacity-50 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-neutral-200/60 focus:ring-offset-2 h-10 font-medium text-white text-sm transition-colors cursor-pointer disabled:pointer-events-none basis-10"
                                     @click="slideOverOpen=true" />
 
                                 <template x-teleport="body">
-                                    <div class="relative z-[99]" x-show="slideOverOpen"
+                                    <div class="z-[99] relative" x-show="slideOverOpen"
                                         @keydown.window.escape="slideOverOpen=false">
                                         <div class="fixed inset-0 bg-black bg-opacity-10" x-show="slideOverOpen"
                                             x-transition.opacity.duration.600ms @click="slideOverOpen = false"></div>
                                         <div class="fixed inset-0 overflow-hidden">
                                             <div class="absolute inset-0 overflow-hidden">
-                                                <div class="fixed inset-y-0 right-0 flex max-w-full pl-10">
+                                                <div class="right-0 fixed inset-y-0 flex pl-10 max-w-full">
                                                     <div class="w-screen max-w-md" x-show="slideOverOpen"
                                                         @click.away="slideOverOpen = false"
                                                         x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -184,20 +185,20 @@
                                                         x-transition:leave-start="translate-x-0"
                                                         x-transition:leave-end="translate-x-full">
                                                         <div
-                                                            class="flex h-full flex-col overflow-y-scroll border-l border-neutral-100/70 bg-white py-5 shadow-lg">
+                                                            class="flex flex-col bg-white shadow-lg py-5 border-neutral-100/70 border-l h-full overflow-y-scroll">
                                                             <div class="px-4 sm:px-5">
-                                                                <div class="flex items-start justify-between pb-1">
+                                                                <div class="flex justify-between items-start pb-1">
 
                                                                     <div class="w-full max-w-24">
                                                                         <img src="{{ asset('img/logo-bni.png') }}"
                                                                             alt="">
                                                                     </div>
 
-                                                                    <div class="ml-3 flex h-auto items-center">
+                                                                    <div class="flex items-center ml-3 h-auto">
                                                                         <button
-                                                                            class="absolute right-0 top-0 z-30 mr-5 mt-4 flex items-center justify-center space-x-1 rounded-md border border-neutral-200 px-3 py-2 text-xs font-medium uppercase text-neutral-600 hover:bg-neutral-100"
+                                                                            class="top-0 right-0 z-30 absolute flex justify-center items-center space-x-1 hover:bg-neutral-100 mt-4 mr-5 px-3 py-2 border border-neutral-200 rounded-md font-medium text-neutral-600 text-xs uppercase"
                                                                             @click="slideOverOpen=false">
-                                                                            <svg class="h-4 w-4"
+                                                                            <svg class="w-4 h-4"
                                                                                 xmlns="http://www.w3.org/2000/svg"
                                                                                 fill="none" viewBox="0 0 24 24"
                                                                                 stroke-width="1.5"
@@ -211,13 +212,13 @@
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            <div class="relative mt-5 flex-1 px-4 sm:px-5">
+                                                            <div class="relative flex-1 mt-5 px-4 sm:px-5">
                                                                 <ul class="flex flex-col gap-8">
                                                                     @foreach ($navItems as $item)
                                                                         <li class="flex flex-col gap-2"
                                                                             @if (!empty($item['subItems'])) x-data="{ open: false }" @endif>
                                                                             @if (!empty($item['subItems']))
-                                                                                <div class="flex cursor-pointer items-center gap-2"
+                                                                                <div class="flex items-center gap-2 cursor-pointer"
                                                                                     @click="open = !open">
                                                                             @endif
                                                                             <a
@@ -225,12 +226,12 @@
                                                                             @if (!empty($item['subItems']))
                                                                                 <div :class="open && 'rotate-90'">
                                                                                     <x-lucide-chevron-right
-                                                                                        class="inline-block h-4 w-4 transition-transform duration-300" />
+                                                                                        class="inline-block w-4 h-4 transition-transform duration-300" />
                                                                                 </div>
                                                             </div>
                                                             @endif
                                                             @if (!empty($item['subItems']))
-                                                                <ul class="ml-4 flex flex-col gap-6" x-show="open"
+                                                                <ul class="flex flex-col gap-6 ml-4" x-show="open"
                                                                     x-transition>
                                                                     @foreach ($item['subItems'] as $subItem)
                                                                         <li class="flex flex-col gap-2">
@@ -259,32 +260,32 @@
 
         {{-- /* ********************** DESKTOP SUBITEMS ********************** */ --}}
         <template x-if="isBreakpoint('lg')">
-            <div class="fixed left-0 z-10 w-dvw duration-200 ease-out" :style="{ 'margin-bottom': -height + 'px' }"
+            <div class="left-0 z-10 fixed w-dvw duration-200 ease-out" :style="{ 'margin-bottom': -height + 'px' }"
                 x-ref="navigationDropdown" x-show="navigationMenuOpen"
                 x-transition:enter="transition ease-out duration-100" x-transition:enter-start="opacity-0 scale-90"
                 x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-100"
                 x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-90"
                 @mouseover="navigationMenuClearCloseTimeout()" @mouseleave="navigationMenuLeave()" x-cloak>
                 <div
-                    class="flex h-auto w-auto justify-center overflow-hidden border border-neutral-200/70 bg-white shadow-sm">
+                    class="flex justify-center bg-white shadow-sm border border-neutral-200/70 w-auto h-auto overflow-hidden">
                     @foreach ($navItems as $item)
                         @if (!empty($item['subItems']))
-                            <div class="relative mx-auto flex w-full max-w-none items-start justify-between gap-x-3 px-2 py-4 max-lg:flex-col lg:max-w-5xl"
+                            <div class="relative flex max-lg:flex-col justify-between items-start gap-x-3 mx-auto px-2 py-4 w-full max-w-none lg:max-w-5xl"
                                 x-show="navigationMenu == '{{ $item['slug'] }}'">
                                 <div class="flex-shrink-0 max-lg:pb-4 lg:basis-56">
                                     <x-event-list-title class="!m-auto !w-full !basis-3/4">
                                         Member By Business Categories
                                     </x-event-list-title>
                                 </div>
-                                <div class="grid gap-6 lg:grid-cols-2">
+                                <div class="gap-6 grid lg:grid-cols-2">
                                     @foreach ($item['subItems'] as $subItem)
-                                        <a class="block rounded px-3.5 py-3 text-sm hover:bg-neutral-100"
+                                        <a class="block hover:bg-neutral-100 px-3.5 py-3 rounded text-sm"
                                             href="{{ $subItem->href ?? '#' }}" @click="navigationMenuClose()">
                                             <span
-                                                class="mb-1 block font-medium text-black">{{ $subItem->name }}</span>
+                                                class="block mb-1 font-medium text-black">{{ $subItem->name }}</span>
                                             @if (!empty($subItem->description))
                                                 <span
-                                                    class="block font-light leading-5 opacity-50">{{ $subItem->description }}</span>
+                                                    class="block opacity-50 font-light leading-5">{{ $subItem->description }}</span>
                                             @endif
                                         </a>
                                     @endforeach
@@ -305,17 +306,17 @@
     @yield('page')
 
     <div
-        class="fixed bottom-[clamp(2rem,20vw,3.5rem)] right-[clamp(0.5rem,5vw,2rem)] z-50 size-[clamp(2rem,12vw,4rem)] drop-shadow-lg transition-all duration-700 ease-in-out hover:bottom-[clamp(3rem,30vw,4.5rem)] hover:drop-shadow-2xl">
+        class="right-[clamp(0.5rem,5vw,2rem)] bottom-[clamp(2rem,20vw,3.5rem)] hover:bottom-[clamp(3rem,30vw,4.5rem)] z-50 fixed drop-shadow-lg hover:drop-shadow-2xl size-[clamp(2rem,12vw,4rem)] transition-all duration-700 ease-in-out">
         <a target="_blank" href="https://api.whatsapp.com/send?phone=628161306769">
             <img src="{{ asset('img/wa.png') }}" alt="whatsapp contact">
         </a>
     </div>
 
-    <footer class="sticky bottom-0 z-40 flex items-center justify-center bg-navy px-2 py-4">
-        <h4 class="inline-flex pt-[0.3rem] text-sm text-white lg:text-base">BNI Magnitude Official Website | Powered By
+    <footer class="bottom-0 z-40 sticky flex justify-center items-center bg-navy px-2 py-4">
+        <h4 class="inline-flex pt-[0.3rem] text-white text-sm lg:text-base">BNI Magnitude Official Website | Powered By
         </h4>
         <a class="inline-flex" href="https://designcub3.com" rel="noopener noreferrer" target="_blank">
-            <img class="ml-2 mt-1 w-24 lg:w-12" src="{{ asset('img/logo.svg') }}" alt="">
+            <img class="mt-1 ml-2 w-24 lg:w-12" src="{{ asset('img/logo.svg') }}" alt="">
             {{-- <img class="ml-2 w-24 lg:w-32" src="{{ asset('img/footer-logo.gif') }}" alt=""> --}}
         </a>
     </footer>
